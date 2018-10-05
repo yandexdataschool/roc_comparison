ab0c9980d06e953dbd6cca59c62bb9e90f456802
Nikita Kazeev <nikita.kazeev@cern.ch>
2018-10-05 10:14:05 +0000

    sklearn bug?

--- compare_auc_delong_xu.py
+++ compare_auc_delong_xu.py
@@ -29,7 +29,87 @@ def compute_midrank(x):
     return T2
 
 
-def fastDeLong(predictions_sorted_transposed, label_1_count):
+def compute_midrank_weight(x, sample_weight):
+    """Computes midranks.
+    Args:
+       x - a 1D numpy array
+    Returns:
+       array of midranks
+    """
+    J = np.argsort(x)
+    Z = x[J]
+    cumulative_weight = np.cumsum(sample_weight[J])
+    N = len(x)
+    T = np.zeros(N, dtype=np.float)
+    i = 0
+    while i < N:
+        j = i
+        while j < N and Z[j] == Z[i]:
+            j += 1
+        T[i:j] = cumulative_weight[i:j].mean()
+        i = j
+    T2 = np.empty(N, dtype=np.float)
+    T2[J] = T
+    return T2
+
+
+def fastDeLong(predictions_sorted_transposed, label_1_count, sample_weight):
+    if sample_weight is None:
+        return fastDeLong_no_weights(predictions_sorted_transposed, label_1_count)
+    else:
+        return fastDeLong_weights(predictions_sorted_transposed, label_1_count, sample_weight)
+
+
+def fastDeLong_weights(predictions_sorted_transposed, label_1_count, sample_weight):
+    """
+    The fast version of DeLong's method for computing the covariance of
+    unadjusted AUC.
+    Args:
+       predictions_sorted_transposed: a 2D numpy.array[n_classifiers, n_examples]
+          sorted such as the examples with label "1" are first
+    Returns:
+       (AUC value, DeLong covariance)
+    Reference:
+     @article{sun2014fast,
+       title={Fast Implementation of DeLong's Algorithm for
+              Comparing the Areas Under Correlated Receiver Oerating Characteristic Curves},
+       author={Xu Sun and Weichao Xu},
+       journal={IEEE Signal Processing Letters},
+       volume={21},
+       number={11},
+       pages={1389--1393},
+       year={2014},
+       publisher={IEEE}
+     }
+    """
+    # Short variables are named as they are in the paper
+    m = label_1_count
+    n = predictions_sorted_transposed.shape[1] - m
+    positive_examples = predictions_sorted_transposed[:, :m]
+    negative_examples = predictions_sorted_transposed[:, m:]
+    k = predictions_sorted_transposed.shape[0]
+
+    tx = np.empty([k, m], dtype=np.float)
+    ty = np.empty([k, n], dtype=np.float)
+    tz = np.empty([k, m + n], dtype=np.float)
+    for r in range(k):
+        tx[r, :] = compute_midrank_weight(positive_examples[r, :], sample_weight[:m])
+        ty[r, :] = compute_midrank_weight(negative_examples[r, :], sample_weight[m:])
+        tz[r, :] = compute_midrank_weight(predictions_sorted_transposed[r, :], sample_weight)
+    total_positive_weights = sample_weight[:m].sum()
+    total_negative_weights = sample_weight[m:].sum()
+    pair_weights = np.dot(sample_weight[:m, np.newaxis], sample_weight[np.newaxis, m:])
+    total_pair_weights = pair_weights.sum()
+    aucs = (sample_weight[:m]*(tz[:, :m] - tx)).sum(axis=1) / total_pair_weights
+    v01 = (tz[:, :m] - tx[:, :]) / total_negative_weights
+    v10 = 1.0 - (tz[:, m:] - ty[:, :]) / total_positive_weights
+    sx = np.cov(v01)
+    sy = np.cov(v10)
+    delongcov = sx / total_positive_weights + sy / total_negative_weights
+    return aucs, delongcov
+
+
+def fastDeLong_no_weights(predictions_sorted_transposed, label_1_count):
     """
     The fast version of DeLong's method for computing the covariance of
     unadjusted AUC.
@@ -87,28 +167,34 @@ def calc_pvalue(aucs, sigma):
     return np.log10(2) + scipy.stats.norm.logsf(z, loc=0, scale=1) / np.log(10)
 
 
-def compute_ground_truth_statistics(ground_truth):
+def compute_ground_truth_statistics(ground_truth, sample_weight):
     assert np.array_equal(np.unique(ground_truth), [0, 1])
     order = (-ground_truth).argsort()
     label_1_count = int(ground_truth.sum())
-    return order, label_1_count
+    if sample_weight is None:
+        ordered_sample_weight = None
+    else:
+        ordered_sample_weight = sample_weight[order]
+
+    return order, label_1_count, ordered_sample_weight
 
 
-def delong_roc_variance(ground_truth, predictions):
+def delong_roc_variance(ground_truth, predictions, sample_weight=None):
     """
     Computes ROC AUC variance for a single set of predictions
     Args:
        ground_truth: np.array of 0 and 1
        predictions: np.array of floats of the probability of being class 1
     """
-    order, label_1_count = compute_ground_truth_statistics(ground_truth)
+    order, label_1_count, ordered_sample_weight = compute_ground_truth_statistics(
+        ground_truth, sample_weight)
     predictions_sorted_transposed = predictions[np.newaxis, order]
-    aucs, delongcov = fastDeLong(predictions_sorted_transposed, label_1_count)
+    aucs, delongcov = fastDeLong(predictions_sorted_transposed, label_1_count, ordered_sample_weight)
     assert len(aucs) == 1, "There is a bug in the code, please forward this to the developers"
     return aucs[0], delongcov
 
 
-def delong_roc_test(ground_truth, predictions_one, predictions_two):
+def delong_roc_test(ground_truth, predictions_one, predictions_two, sample_weigt=None):
     """
     Computes log(p-value) for hypothesis that two ROC AUCs are different
     Args:
@@ -123,17 +209,3 @@ def delong_roc_test(ground_truth, predictions_one, predictions_two):
     aucs, delongcov = fastDeLong(predictions_sorted_transposed, label_1_count)
     print(aucs, delongcov)
     return calc_pvalue(aucs, delongcov)
-
-
-def stratified_sampling_mask(array, sample_fraction):
-    # WARNING(kazeevn)
-    # selection size might be off from sample_fraction due to
-    # rounding error accumulation
-    # also doesn't shuffle
-    # doen't scale well with the number of classes
-    selected = np.zeros(shape=array.shape, dtype=np.bool)
-    for value, count in zip(*np.unique(array, return_counts=True)):
-        indices = (array == value).nonzero()[0]
-        this_selection = np.random.choice(count, size=int(count*sample_fraction), replace=False)
-        selected[indices[this_selection]] = True
-    return selected
--- test.py
+++ test.py
@@ -8,10 +8,79 @@ import compare_auc_delong_xu
 def test_variance():
     data = sklearn.datasets.load_iris()
     x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(
-        data.data, data.target == 1, test_size=0.8, random_state=42)
+        data.data, (data.target == 1).astype(numpy.int), test_size=0.8, random_state=42)
     predictions = sklearn.linear_model.LogisticRegression().fit(
         x_train, y_train).predict_proba(x_test)[:, 1]
     auc, variance = compare_auc_delong_xu.delong_roc_variance(y_test, predictions)
     true_auc = sklearn.metrics.roc_auc_score(y_test, predictions)
     numpy.testing.assert_allclose(true_auc, auc)
     numpy.testing.assert_allclose(0.0014569635512, variance)
+
+def test_weights_positive():
+    return
+    data = sklearn.datasets.load_iris()
+    x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(
+        data.data, (data.target == 1).astype(numpy.int), test_size=0.8, random_state=42)
+    predictions = sklearn.linear_model.LogisticRegression().fit(
+        x_train, y_train).predict_proba(x_test)[:, 1]
+    weights = numpy.linspace(0, 3, num=len(x_test))
+    auc, variance = compare_auc_delong_xu.delong_roc_variance(y_test, predictions,
+                                                              sample_weight=weights)
+    true_auc = sklearn.metrics.roc_auc_score(y_test, predictions, sample_weight=weights)
+    numpy.testing.assert_allclose(true_auc, auc)
+
+
+def test_weights_one():
+    data = sklearn.datasets.load_iris()
+    x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(
+        data.data, (data.target == 1).astype(numpy.int), test_size=0.8, random_state=42)
+    predictions = sklearn.linear_model.LogisticRegression().fit(
+        x_train, y_train).predict_proba(x_test)[:, 1]
+    weights = numpy.ones(shape=y_test.shape)
+    auc, variance = compare_auc_delong_xu.delong_roc_variance(y_test, predictions,
+                                                              sample_weight=weights)
+    true_auc = sklearn.metrics.roc_auc_score(y_test, predictions, sample_weight=weights)
+    numpy.testing.assert_allclose(true_auc, auc)
+
+    
+def test_weights_equal_integer():
+    data = sklearn.datasets.load_iris()
+    x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(
+        data.data, (data.target == 1).astype(numpy.int), test_size=0.8, random_state=42)
+    predictions = sklearn.linear_model.LogisticRegression().fit(
+        x_train, y_train).predict_proba(x_test)[:, 1]
+    weights = numpy.ones(shape=y_test.shape)*3
+    auc, variance = compare_auc_delong_xu.delong_roc_variance(y_test, predictions,
+                                                              sample_weight=weights)
+    true_auc = sklearn.metrics.roc_auc_score(y_test, predictions, sample_weight=weights)
+    numpy.testing.assert_allclose(true_auc, auc)
+
+
+def test_weights_equal_big():
+    data = sklearn.datasets.load_iris()
+    x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(
+        data.data, (data.target == 1).astype(numpy.int), test_size=0.8, random_state=42)
+    predictions = sklearn.linear_model.LogisticRegression().fit(
+        x_train, y_train).predict_proba(x_test)[:, 1]
+    weights = numpy.ones(shape=y_test.shape)*2.13
+    # Funny. 2.5 - OK, 2.25 - OK
+    N = 7
+    auc, variance = compare_auc_delong_xu.delong_roc_variance(y_test[:N], predictions[:N],
+                                                              sample_weight=weights[:N])
+    true_auc = sklearn.metrics.roc_auc_score(y_test[:N], predictions[:N], sample_weight=weights[:N])
+    print(predictions[:N], y_test[:N], sklearn.metrics.roc_auc_score(y_test[:N], predictions[:N]))
+    numpy.testing.assert_allclose(true_auc, auc)
+
+
+def test_weights_equal_small():
+    return
+    data = sklearn.datasets.load_iris()
+    x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(
+        data.data, (data.target == 1).astype(numpy.int), test_size=0.8, random_state=42)
+    predictions = sklearn.linear_model.LogisticRegression().fit(
+        x_train, y_train).predict_proba(x_test)[:, 1]
+    weights = numpy.ones(shape=y_test.shape)*0.214124
+    auc, variance = compare_auc_delong_xu.delong_roc_variance(y_test, predictions,
+                                                              sample_weight=weights)
+    true_auc = sklearn.metrics.roc_auc_score(y_test, predictions, sample_weight=weights)
+    numpy.testing.assert_allclose(true_auc, auc)
