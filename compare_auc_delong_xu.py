a83f31d2337f61e755d63562a503b0148f3751d8
Nikita Kazeev <nikit.kazeev@cern.ch>
2019-07-01 22:57:09 +0300

    Removed incorrect weighted computations

--- compare_auc_delong_xu.py
+++ compare_auc_delong_xu.py
@@ -53,62 +53,7 @@ def compute_midrank_weight(x, sample_weight):
     return T2
 
 
-def fastDeLong(predictions_sorted_transposed, label_1_count, sample_weight):
-    if sample_weight is None:
-        return fastDeLong_no_weights(predictions_sorted_transposed, label_1_count)
-    else:
-        return fastDeLong_weights(predictions_sorted_transposed, label_1_count, sample_weight)
-
-
-def fastDeLong_weights(predictions_sorted_transposed, label_1_count, sample_weight):
-    """
-    The fast version of DeLong's method for computing the covariance of
-    unadjusted AUC.
-    Args:
-       predictions_sorted_transposed: a 2D numpy.array[n_classifiers, n_examples]
-          sorted such as the examples with label "1" are first
-    Returns:
-       (AUC value, DeLong covariance)
-    Reference:
-     @article{sun2014fast,
-       title={Fast Implementation of DeLong's Algorithm for
-              Comparing the Areas Under Correlated Receiver Oerating Characteristic Curves},
-       author={Xu Sun and Weichao Xu},
-       journal={IEEE Signal Processing Letters},
-       volume={21},
-       number={11},
-       pages={1389--1393},
-       year={2014},
-       publisher={IEEE}
-     }
-    """
-    # Short variables are named as they are in the paper
-    m = label_1_count
-    n = predictions_sorted_transposed.shape[1] - m
-    positive_examples = predictions_sorted_transposed[:, :m]
-    negative_examples = predictions_sorted_transposed[:, m:]
-    k = predictions_sorted_transposed.shape[0]
-
-    tx = np.empty([k, m], dtype=np.float)
-    ty = np.empty([k, n], dtype=np.float)
-    tz = np.empty([k, m + n], dtype=np.float)
-    for r in range(k):
-        tx[r, :] = compute_midrank_weight(positive_examples[r, :], sample_weight[:m])
-        ty[r, :] = compute_midrank_weight(negative_examples[r, :], sample_weight[m:])
-        tz[r, :] = compute_midrank_weight(predictions_sorted_transposed[r, :], sample_weight)
-    total_positive_weights = sample_weight[:m].sum()
-    total_negative_weights = sample_weight[m:].sum()
-    total_pair_weights = (sample_weight[:m] * total_negative_weights).sum()
-    aucs = (sample_weight[:m]*(tz[:, :m] - tx)).sum(axis=1) / total_pair_weights
-    v01 = (tz[:, :m] - tx[:, :]) / total_negative_weights
-    v10 = 1. - (tz[:, m:] - ty[:, :]) / total_positive_weights
-    sx = np.cov(v01)
-    sy = np.cov(v10)
-    delongcov = sx / m + sy / n
-    return aucs, delongcov
-
-
-def fastDeLong_no_weights(predictions_sorted_transposed, label_1_count):
+def fastDeLong(predictions_sorted_transposed, label_1_count):
     """
     The fast version of DeLong's method for computing the covariance of
     unadjusted AUC.
@@ -166,7 +111,7 @@ def calc_pvalue(aucs, sigma):
     return np.log10(2) + scipy.stats.norm.logsf(z, loc=0, scale=1) / np.log(10)
 
 
-def compute_ground_truth_statistics(ground_truth, sample_weight):
+def compute_ground_truth_statistics(ground_truth, sample_weight=None):
     assert np.array_equal(np.unique(ground_truth), [0, 1])
     order = (-ground_truth).argsort()
     label_1_count = int(ground_truth.sum())
@@ -178,13 +123,14 @@ def compute_ground_truth_statistics(ground_truth, sample_weight):
     return order, label_1_count, ordered_sample_weight
 
 
-def delong_roc_variance(ground_truth, predictions, sample_weight=None):
+def delong_roc_variance(ground_truth, predictions):
     """
     Computes ROC AUC variance for a single set of predictions
     Args:
        ground_truth: np.array of 0 and 1
        predictions: np.array of floats of the probability of being class 1
     """
+    sample_weight = None
     order, label_1_count, ordered_sample_weight = compute_ground_truth_statistics(
         ground_truth, sample_weight)
     predictions_sorted_transposed = predictions[np.newaxis, order]
@@ -193,7 +139,7 @@ def delong_roc_variance(ground_truth, predictions, sample_weight=None):
     return aucs[0], delongcov
 
 
-def delong_roc_test(ground_truth, predictions_one, predictions_two, sample_weigt=None):
+def delong_roc_test(ground_truth, predictions_one, predictions_two):
     """
     Computes log(p-value) for hypothesis that two ROC AUCs are different
     Args:
@@ -203,8 +149,8 @@ def delong_roc_test(ground_truth, predictions_one, predictions_two, sample_weigt
        predictions_two: predictions of the second model,
           np.array of floats of the probability of being class 1
     """
+    sample_weight = None
     order, label_1_count = compute_ground_truth_statistics(ground_truth)
     predictions_sorted_transposed = np.vstack((predictions_one, predictions_two))[:, order]
     aucs, delongcov = fastDeLong(predictions_sorted_transposed, label_1_count)
-    print(aucs, delongcov)
     return calc_pvalue(aucs, delongcov)
